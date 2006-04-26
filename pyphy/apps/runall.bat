bb3ede6a8ce2c626312a1f2725cf12d363628cc6
plewis <plewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2006-04-26 02:50:52 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas@26 b774380d-c90d-0410-997c-aec342e5d6fd

--- pyphy/apps/runall.bat
+++ pyphy/apps/runall.bat
@@ -9,7 +9,7 @@ echo *** Running ExplorePrior *** >> runall_diffs.txt
 echo **************************** >> runall_diffs.txt
 echo. >> runall_diffs.txt
 cd ExplorePrior
-REM python ExplorePrior.py
+python ExplorePrior.py
 fc nodata.nex.p refExplorePrior\nodata.nex.p >> ..\runall_diffs.txt 
 if errorlevel 1 (goto abort)
 fc nodata.nex.t refExplorePrior\nodata.nex.t >> ..\runall_diffs.txt
@@ -26,7 +26,7 @@ echo *** Running FixedParams *** >> runall_diffs.txt
 echo *************************** >> runall_diffs.txt
 echo. >> runall_diffs.txt
 cd FixedParams
-REM python FixedParams.py
+python FixedParams.py
 fc params.p ref_output\params.p >> ..\runall_diffs.txt
 if errorlevel 1 (goto abort)
 fc trees.t ref_output\trees.t >> ..\runall_diffs.txt
@@ -43,7 +43,7 @@ echo *** Running GelfandGhosh *** >> runall_diffs.txt
 echo **************************** >> runall_diffs.txt
 echo. >> runall_diffs.txt
 cd GelfandGhosh
-REM python GelfandGhosh.py
+python GelfandGhosh.py
 fc ggout.txt reference_output\ggout.txt >> ..\runall_diffs.txt
 if errorlevel 1 (goto abort)
 cd ..
