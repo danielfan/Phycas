251c29e1984dad4076a5622e71cb44a6ca96184e
plewis <plewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2006-07-12 01:00:45 +0000

    Added some functionality oto CipresPhycas.py
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas@113 b774380d-c90d-0410-997c-aec342e5d6fd

--- pyphy/apps/CipresPhycas/CipresPhycas.py
+++ pyphy/apps/CipresPhycas/CipresPhycas.py
@@ -15,9 +15,11 @@ class CipresPhycas(CipresIDL_api1__POA.AsyncTreeInfer, SimpleServer, Phycas):
         Phycas.__init__(self)
         SimpleServer.__init__(self, registry)
         self.registry = registry
-        self.nTax = 0
-        self.tree = CipresTree('(1,2,(3,4))')
-        self.nTrees = 10
+        self.trees = []
+        self.curr_tree_pos = None
+        #self.nTax = 0
+        #self.tree = CipresTree('(1,2,(3,4))')
+        #self.nTrees = 10
 
     # Scriptable interface    
     def execute(self, cmd):
@@ -38,37 +40,100 @@ class CipresPhycas(CipresIDL_api1__POA.AsyncTreeInfer, SimpleServer, Phycas):
     # AsyncTreeIterator
     def getFirstTree(self):
         _LOG.debug('CipresPhycas.getFirstTree')
-        return self.tree
+        if self.getNumTrees() > 0:
+            self.curr_tree_pos = 0
+            return self.trees[0]
+        else:
+            RuntimeError, 'getFirstTree failed because there are currently no trees to supply'
     def getLastTree(self):
         _LOG.debug('CipresPhycas.getLastTree')
-        return self.tree
+        n = self.getNumTrees()
+        if n > 0:
+            self.curr_tree_pos = n - 1
+            return self.trees[self.curr_tree_pos]
+        else:
+            RuntimeError, 'getLastTree failed because there are currently no trees to supply'
     def getNextTree(self):
         _LOG.debug('CipresPhycas.getNextTree')
-        return self.tree
+        if self.curr_tree_pos and self.curr_tree_pos + 1 < self.getNumTrees():
+            self.curr_tree_pos += 1
+            return self.trees[self.curr_tree_pos]
+        else:
+            if self.curr_tree_pos:
+                errmsg = 'getNextTree failed because there are currently no trees beyond the current tree (%d)' % self.curr_tree_pos
+            else:
+                errmsg = 'getNextTree failed because there are currently no trees to supply'
+            RuntimeError, errmsg
     def getPreviousTree(self):
         _LOG.debug('CipresPhycas.getPreviousTree')
-        return self.tree
+        if self.curr_tree_pos and (self.curr_tree_pos > 0):
+            self.curr_tree_pos -= 1
+            return self.trees[self.curr_tree_pos]
+        else:
+            if self.curr_tree_pos:
+                errmsg = 'getPreviousTree failed because the current tree is the first tree'
+            else:
+                errmsg = 'getPreviousTree failed because there are currently no trees to supply'
+            RuntimeError, errmsg
     def getTreeByIndex(self, whichTree):
         _LOG.debug('CipresPhycas.getTreeByIndex(%d)' % whichTree)
-        return self.tree
+        if self.curr_tree_pos and (whichTree >= 0) and (whichTree < self.getNumTrees()):
+            self.curr_tree_pos = whichTree
+            return self.trees[self.curr_tree_pos]
+        else:
+            if not self.curr_tree_pos:
+                errmsg = 'getTreeByIndex failed because there are currently no trees to supply'
+            elif whichTree >= self.getNumTrees():
+                errmsg = 'getTreeByIndex failed because the requested tree index (%d) is greater than the current number of trees (%d)' % (whichTree, len(self.trees))
+            else:
+                errmsg = 'getTreeByIndex failed because the requested tree index (%d) was less than 0' % whichTree
+            RuntimeError, errmsg
     def pop(self):
         _LOG.debug('CipresPhycas.pop')
-        return self.tree
+        if self.curr_tree_pos:
+            t = self.trees.pop()
+            n = self.getNumTrees()
+            if n > 0:
+                self.curr_tree_pos = n - 1
+            else:
+                self.curr_tree_pos = None
+            return t
+        else:
+            RuntimeError, 'pop failed because there are currently no trees to supply'
     def push(self, treeParam):
         raise RuntimeError, 'Phycas does not accept trees via the push method!'
     def shift(self):
         _LOG.debug('CipresPhycas.shift')
-        return self.tree
+        if self.curr_tree_pos:
+            t = self.trees.pop(0)
+            n = self.getNumTrees()
+            if n > 0:
+                self.curr_tree_pos = 0
+            else:
+                self.curr_tree_pos = None
+            return t
+        else:
+            RuntimeError, 'shift failed because there are currently no trees to supply'
     def unshift(self, treeParam):
         raise RuntimeError, 'Phycas does not accept trees via the unshift method!'
     def getNumTrees(self):
         _LOG.debug('CipresPhycas.getNumTrees')
-        return self.nTrees
+        return len(self.trees)
     def exists(self, treeInd):
         _LOG.debug('CipresPhycas.exists %d ' % treeInd)
-        return treeInd < self.nTrees
+        return treeInd < self.getNumTrees()
 
-
+    # Phycas overrides
+    def recordSample(self, cycle, lnL = 0.0):
+        #---+----|----+----|----+----|----+----|----+----|----+----|----+----|
+        """
+        Calls base class version, then stores tree description in list so that
+        trees can be supplied when requested.
+        """
+        Phycas.recordSample(self, cycle, lnL)
+        self.trees.append(self.tree.makeNewick())
+        if not self.curr_tree_pos:
+            self.curr_tree_pos = 0
 
 if __name__=='__main__':
     argv = sys.argv
