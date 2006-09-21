3ae02856660b16c1d69d004d133f44d0c5918b96
plewis <plewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2006-09-21 00:54:57 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@214 b774380d-c90d-0410-997c-aec342e5d6fd

--- phypy/phypy/Tests/runall.sh
+++ phypy/phypy/Tests/runall.sh
@@ -6,134 +6,132 @@ echo "Shell script aborted because of failed example"
 exit 1
 }
 
-rm -f runall_diffs.txt
+rm -f $TESTDIR/runall_diffs.txt
 
 TESTDIR=`pwd`
-echo $TESTDIR
-exit 0
 cd ../Examples
 
-echo "****************************" >> runall_diffs.txt
-echo "*** Running ExplorePrior ***" >> runall_diffs.txt
-echo "****************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo "****************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running ExplorePrior ***" >> $TESTDIR/runall_diffs.txt
+echo "****************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd ExplorePrior
 python ExplorePrior.py
-diff nodata.nex.p reference_output/nodata.nex.p >> ../runall_diffs.txt 
+diff nodata.nex.p reference_output/nodata.nex.p >> $TESTDIR/runall_diffs.txt 
 if [ $? -ne 0 ] 
 then
   abort
 fi
-diff nodata.nex.t reference_output/nodata.nex.t >> ../runall_diffs.txt
+diff nodata.nex.t reference_output/nodata.nex.t >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
 cd ..
 
-echo " " >> runall_diffs.txt
-echo "***************************" >> runall_diffs.txt
-echo "*** Running FixedParams ***" >> runall_diffs.txt
-echo "***************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
+echo "***************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running FixedParams ***" >> $TESTDIR/runall_diffs.txt
+echo "***************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd FixedParams
 python FixedParams.py
-diff params.p reference_output/params.p >> ../runall_diffs.txt
+diff params.p reference_output/params.p >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
-diff trees.t reference_output/trees.t >> ../runall_diffs.txt
+diff trees.t reference_output/trees.t >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
 cd ..
 
-echo " " >> runall_diffs.txt
-echo "****************************" >> runall_diffs.txt
-echo "*** Running GelfandGhosh ***" >> runall_diffs.txt
-echo "****************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
+echo "****************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running GelfandGhosh ***" >> $TESTDIR/runall_diffs.txt
+echo "****************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd GelfandGhosh
 python GelfandGhosh.py
-diff ggout.txt reference_output/ggout.txt >> ../runall_diffs.txt
+diff ggout.txt reference_output/ggout.txt >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
 cd ..
 
-echo " " >> runall_diffs.txt
-echo "******************************" >> runall_diffs.txt
-echo "*** Running LikelihoodTest ***" >> runall_diffs.txt
-echo "******************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
+echo "******************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running LikelihoodTest ***" >> $TESTDIR/runall_diffs.txt
+echo "******************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd LikelihoodTest
 python LikelihoodTest.py
-diff simulated.nex reference_output/simulated.nex >> ../runall_diffs.txt
+diff simulated.nex reference_output/simulated.nex >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
-diff check.nex reference_output/check.nex >> ../runall_diffs.txt
+diff check.nex reference_output/check.nex >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
 cd ..
 
-#echo " " >> runall_diffs.txt
-#echo "**********************" >> runall_diffs.txt
-#echo "*** Running Phycas ***" >> runall_diffs.txt
-#echo "**********************" >> runall_diffs.txt
-#echo " " >> runall_diffs.txt
+#echo " " >> $TESTDIR/runall_diffs.txt
+#echo "**********************" >> $TESTDIR/runall_diffs.txt
+#echo "*** Running Phycas ***" >> $TESTDIR/runall_diffs.txt
+#echo "**********************" >> $TESTDIR/runall_diffs.txt
+#echo " " >> $TESTDIR/runall_diffs.txt
 #cd Phycas
 #python Phycas.py
-#diff green.nex.p reference_output/green.nex.p >> ../runall_diffs.txt 
+#diff green.nex.p reference_output/green.nex.p >> $TESTDIR/runall_diffs.txt 
 #if [ $? -ne 0 ] 
 #then
 #  abort
 #fi
-#diff green.nex.t reference_output/green.nex.t >> ../runall_diffs.txt
+#diff green.nex.t reference_output/green.nex.t >> $TESTDIR/runall_diffs.txt
 #if [ $? -ne 0 ] 
 #then
 #  abort
 #fi
 #cd ..
 
-echo " " >> runall_diffs.txt
-echo "**************************" >> runall_diffs.txt
-echo "*** Running Polytomies ***" >> runall_diffs.txt
-echo "**************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
+echo "**************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running Polytomies ***" >> $TESTDIR/runall_diffs.txt
+echo "**************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd Polytomies
 python Polytomies.py
-diff analHKY.nex.p reference_output/analHKY.nex.p >> ../runall_diffs.txt
+diff analHKY.nex.p reference_output/analHKY.nex.p >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
-diff analHKY.nex.t reference_output/analHKY.nex.t >> ../runall_diffs.txt
+diff analHKY.nex.t reference_output/analHKY.nex.t >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
-diff simHKY.nex reference_output/simHKY.nex >> ../runall_diffs.txt
+diff simHKY.nex reference_output/simHKY.nex >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
 fi
 cd ..
 
-echo " " >> runall_diffs.txt
-echo "*************************" >> runall_diffs.txt
-echo "*** Running Simulator ***" >> runall_diffs.txt
-echo "*************************" >> runall_diffs.txt
-echo " " >> runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
+echo "*************************" >> $TESTDIR/runall_diffs.txt
+echo "*** Running Simulator ***" >> $TESTDIR/runall_diffs.txt
+echo "*************************" >> $TESTDIR/runall_diffs.txt
+echo " " >> $TESTDIR/runall_diffs.txt
 cd Simulator
 python Simulator.py
-diff simulated.nex reference_output/simulated.nex >> ../runall_diffs.txt
+diff simulated.nex reference_output/simulated.nex >> $TESTDIR/runall_diffs.txt
 if [ $? -ne 0 ] 
 then
   abort
