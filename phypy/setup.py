5f9bf3f671c9653be975b8cda1dec2cbec16ea01
plewis <plewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2006-10-12 17:38:00 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@230 b774380d-c90d-0410-997c-aec342e5d6fd

--- phypy/setup.py
+++ phypy/setup.py
@@ -52,7 +52,7 @@ data_linux_only = ['Tests/cleanall.bat',
                   ]
 data_linux_only.extend(data_all)
 
-windows_package_data = {'phypy.Conversions': ['*.pyd'],
+windows_package_data = {'phypy.Conversions': ['*.pyd','*.dll'],
                     'phypy.DataMatrix': ['*.pyd'],
                     'phypy.Likelihood': ['*.pyd'],
                     'phypy.ProbDist': ['*.pyd'],
@@ -68,7 +68,7 @@ linux_package_data = {'phypy.Conversions': ['*.so'],
                     'phypy.ReadNexus': ['*.so']}
 linux_package_data.update({'phypy': data_linux_only})
       
-setup(name='phypy',
+setup(name='Phycas',
       version='1.0',
       description='Phycas: Python Software for Phylogenetic Analysis',
       author='Mark T. Holder, Paul O. Lewis and David L. Swofford',
@@ -85,78 +85,3 @@ setup(name='phypy',
       package_data=windows_package_data
       #package_data = linux_package_data
      )
-
-#      package_data={'phypy.Conversions': ['*.pyd'],
-#                    'phypy.DataMatrix': ['*.pyd'],
-#                   'phypy.Likelihood': ['*.pyd'],
-#                    'phypy.ProbDist': ['*.pyd'],
-#                    'phypy.Phylogeny': ['*.pyd'],
-#                    'phypy.ReadNexus': ['*.pyd'],
-#                    'phypy': ['Tests/doctestall.py',
-#                              # 'Tests/cleanall.bat',
-#                              # 'Tests/doctestall.bat',
-#                              # 'Tests/runall.bat',
-#                              'Tests/runall.sh',
-#                              'Examples/Data/green.nex',
-#                              'Examples/Data/nyldna4.nex',
-#                              'Examples/ExplorePrior/__init__.py',
-#                              'Examples/ExplorePrior/ExplorePrior.py',
-#                              'Examples/ExplorePrior/reference_output/nodata.nex.p',
-#                              'Examples/ExplorePrior/reference_output/nodata.nex.t',
-#                              'Examples/FixedParams/__init__.py',
-#                              'Examples/FixedParams/FixedParams.py',
-#                              'Examples/FixedParams/reference_output/params.p',
-#                              'Examples/FixedParams/reference_output/trees.t',
-#                              'Examples/GelfandGhosh/__init__.py',
-#                              'Examples/GelfandGhosh/GelfandGhosh.py',
-#                              'Examples/GelfandGhosh/reference_output/analHKY.nex.p',
-#                              'Examples/GelfandGhosh/reference_output/analHKY.nex.t',
-#                              'Examples/GelfandGhosh/reference_output/analHKYflex.nex.p',
-#                              'Examples/GelfandGhosh/reference_output/analHKYflex.nex.t',
-#                              'Examples/GelfandGhosh/reference_output/analHKYg.nex.p',
-#                              'Examples/GelfandGhosh/reference_output/analHKYg.nex.t',
-#                              'Examples/GelfandGhosh/reference_output/ggout.txt',
-#                              'Examples/LikelihoodTest/__init__.py',
-#                              'Examples/LikelihoodTest/LikelihoodTest.py',
-#                              'Examples/LikelihoodTest/reference_output/check.nex',
-#                              'Examples/LikelihoodTest/reference_output/simulated.nex',
-#                              'Examples/Polytomies/__init__.py',
-#                              'Examples/Polytomies/Polytomies.py',
-#                              'Examples/Polytomies/reference_output/analHKY.nex.p',
-#                              'Examples/Polytomies/reference_output/analHKY.nex.t',
-#                              'Examples/Polytomies/reference_output/simHKY.nex',
-#                              'Examples/Simulator/__init__.py',
-#                              'Examples/Simulator/Simulator.py',
-#                              'Examples/Simulator/reference_output/simulated.nex',
-#                              'Examples/*.py',
-#                              ],
-#                   }
-
-#from setuptools import setup, find_packages
-#setup(
-#    name = "phypy",
-#    version = "1.0",
-#    packages = find_packages(),
-#    #scripts = ['say_hello.py'],
-#
-#    # Project uses reStructuredText, so ensure that the docutils get
-#    # installed or upgraded on the target machine
-#    #install_requires = ['docutils>=0.3'],
-#
-#    #package_data = {
-#    #    # If any package contains *.txt or *.rst files, include them:
-#    #    '': ['*.txt', '*.rst'],
-#    #    # And include any *.msg files found in the 'hello' package, too:
-#    #    'hello': ['*.msg'],
-#    #}
-#
-#    # metadata for upload to PyPI
-#    author = "Mark T. Holder, Paul O. Lewis, David L. Swofford",
-#    author_email = "phycoids@phycas.org",
-#    description = "PhyPy: Python Software for Phylogenetic Analysis",
-#    license = "PSF",
-#    keywords = "phypy phycas Bayesian phylogenetics phylogeny likelihood",
-#    url = "http://www.phypy.org/",   # project home page, if any
-#
-#    # could also include long_description, download_url, classifiers, etc.
-#)
