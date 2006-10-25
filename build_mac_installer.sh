0d2a3ca46c16b0f45efab65d4f74f7ab2062b2bb
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2006-10-25 20:00:16 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@271 b774380d-c90d-0410-997c-aec342e5d6fd

--- build_mac_installer.sh
+++ build_mac_installer.sh
@@ -5,13 +5,16 @@
 #   3. Make sure "package_data=windows_package_data" is uncommented in setup.py
 #   4. Change --target-version below to reflect correct Python target version
 #   5. Run this batch file to generate installer (which will be in dist folder)
-
+set -x
 rm -rf build
+cd phypy
+python dojam.py || exit
+cd ..
 cp phypy/bin/boost/libs/python/build/libboost_python.dylib/darwin/release/shared-linkable-true/libboost_python.dylib phypy/phypy/Conversions
 if test -e "$PYTHON_ROOT/bin/bdist_mpkg" ;
 then
     python setup.py build --compiler=fake
-    "$PYTHON_ROOT/bin/bdist_mpkg" --skip-build --open -k
+    "$PYTHON_ROOT/bin/bdist_mpkg" --skip-build -k
 else
     echo "bdist_mpkp is required.  see http://cheeseshop.python.org/pypi/bdist_mpkg/"
     exit 1
