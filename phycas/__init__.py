ae5e4cff249cccd15c09c15247e4b12b765b655b
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2007-10-24 00:50:23 +0000

    added hook for reading ~/.phycas/startup.py (if it exists) when phycas is imported.
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@452 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/__init__.py
+++ phycas/__init__.py
@@ -17,5 +17,11 @@ import Phylogeny
 import ProbDist
 import ReadNexus
 from Phycas import Phycas
-
+_user_ini_checked = False
+if not _user_ini_checked:
+    import os
+    _user_ini_checked = True
+    p = os.path.expanduser("~/.phycas/startup.py")
+    if os.path.exists(p):
+        execfile(p)
 #print 'importing phycas...'
