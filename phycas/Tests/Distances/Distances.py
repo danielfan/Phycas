38daf87b5b591cc53511a61d31c33167303d2049
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2007-10-24 02:53:29 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@455 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Tests/Distances/Distances.py
+++ phycas/Tests/Distances/Distances.py
@@ -1,6 +1,6 @@
 #   #!/usr/bin/env python
 
-import sys
+import sys,os
 from phycas import *
 #from phycas.Constants import *
 
@@ -33,9 +33,11 @@ from phycas import *
 # 		print "%5d%5d%10d" % (i, j, distance[i,j])
 
 p = Phycas()
-p.data_file_name = "../Data/green.nex"
+d = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+p.data_file_name = os.path.join(d,"Data", "green.nex")
 p.default_model = 'jc'
 p.ncycles = 100000
+
 p.random_seed = 13579
 
 #p.readDataFromFile()
