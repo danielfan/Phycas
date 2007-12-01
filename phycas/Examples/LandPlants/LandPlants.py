31d32a028c051b59861039635e011f01cbd7c104
plewis <plewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2007-12-01 00:28:23 +0000

    Set up LandPlants.py example to explore model used by Yang and Rannala
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@534 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Examples/LandPlants/LandPlants.py
+++ phycas/Examples/LandPlants/LandPlants.py
@@ -4,21 +4,23 @@ import os.path,shutil
 
 data_file_name = 'Yang_and_Rannala_Karol.nex'
 rnseed = 15397
-ncycles = 20000
-samplefreq = 20
+ncycles = 21000
+samplefreq = 10
 lsweight = 100
 nreps = 1
-prior_means = [10**x for x in range(-8,4)]
+external_prior_mean = 0.1
+internal_prior_means = [10**x for x in range(-8,4)]
 
 ####################################################################
 #################### Analyze the simulated data ####################
 ####################################################################
-def analyze(prior_mean):
+def analyze(internal_prior_mean):
     analyzer = Phycas()
     analyzer.random_seed = rnseed
-    analyzer.default_model = 'jc'
-    analyzer.num_rates = 1
-    analyzer.edgelen_dist = ProbDist.ExponentialDist(1.0/prior_mean)
+    analyzer.default_model = 'hky'
+    analyzer.num_rates = 5
+    analyzer.external_edgelen_dist = ProbDist.ExponentialDist(1.0/external_prior_mean)
+    analyzer.internal_edgelen_dist = ProbDist.ExponentialDist(1.0/internal_prior_mean)
     analyzer.using_hyperprior = False 
     analyzer.allow_polytomies = False
     analyzer.data_file_name = data_file_name
@@ -30,13 +32,13 @@ def analyze(prior_mean):
     analyzer.gg_kvect = [1.0]
     analyzer.gg_outfile = None
     analyzer.ls_move_weight = lsweight
-    #analyzer.mcmc()
+    analyzer.mcmc()
     analyzer.gg_bin_patterns = True
     analyzer.gg_nreps = nreps
     analyzer.gg_pfile = data_file_name + '.p'
     analyzer.gg_tfile = data_file_name + '.t'
-    shutil.copyfile(analyzer.gg_pfile, '%.9f_%s' % (prior_mean, analyzer.gg_pfile))
-    shutil.copyfile(analyzer.gg_tfile, '%.9f_%s' % (prior_mean, analyzer.gg_tfile))
+    shutil.copyfile(analyzer.gg_pfile, '%.9f_%s' % (internal_prior_mean, analyzer.gg_pfile))
+    shutil.copyfile(analyzer.gg_tfile, '%.9f_%s' % (internal_prior_mean, analyzer.gg_tfile))
     analyzer.gg()
     return (analyzer.gg_Pm, analyzer.gg_Gm[0])
 
@@ -46,11 +48,11 @@ if __name__ == '__main__':
     outf.write('prior mean\tPm\tGm\tDm\n')    # '\t' means tab, '\n' means newline
 
     # For each value of prior mean, perform an MCMC analysis
-    for mean in prior_means:
+    for mean in internal_prior_means:
         Pm, Gm = analyze(mean)
         Dm = Pm + Gm
         print '**'
-        print '** Analysis with prior mean',mean,'yielded Pm =',Pm,', Gm =',Gm
+        print '** Analysis with internal prior mean',mean,'yielded Pm =',Pm,', Gm =',Gm
         print '**'
 
         # Write out a line comprising four values separated by tabs
