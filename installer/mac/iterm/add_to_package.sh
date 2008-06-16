1a1c8af91f83499caf6f79ae91717a2ff998551d
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-16 19:36:17 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@630 b774380d-c90d-0410-997c-aec342e5d6fd

---
+++ installer/mac/iterm/add_to_package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+for i in Development Deployment ; 
+do
+	cp PhycasGUIexecFromGUI.sh ./build/$i/PhycasGUI.app/Contents/MacOS/ 
+	chmod +x ./build/$i/PhycasGUI.app/Contents/MacOS/PhycasGUIexecFromGUI.sh 
+	cp -r $PHYCAS_ROOT/phycas  ./build/$i/PhycasGUI.app/Contents/Resources/
+done
