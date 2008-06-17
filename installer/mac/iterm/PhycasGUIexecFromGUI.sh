355c047f0d40378248a0768d76c087e9314effcd
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-17 15:44:21 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@638 b774380d-c90d-0410-997c-aec342e5d6fd

--- installer/mac/iterm/PhycasGUIexecFromGUI.sh
+++ installer/mac/iterm/PhycasGUIexecFromGUI.sh
@@ -21,7 +21,39 @@ else
 	echo "$0: $phycas_dir does not exist!"
 	exit 1
 fi
-export PYTHONPATH
-export DYLD_LIBRARY_PATH
-python -i -c "from phycas import *"
+env_settings_path="$HOME/.phycas/phycas_gui_env.sh"
+if ! test -d "$HOME/.phycas"
+then
+	mkdir "$HOME/.phycas"
+fi
+if test -d "$HOME/.phycas"
+then
+	echo "#!/bin/sh" > "$env_settings_path"
+	echo "PYTHONPATH=\"$PYTHONPATH\"" >> "$env_settings_path"
+	echo "DYLD_LIBRARY_PATH=\"$DYLD_LIBRARY_PATH\"" >> "$env_settings_path"
+	echo "export PYTHONPATH" >> "$env_settings_path"
+	echo "export DYLD_LIBRARY_PATH" >> "$env_settings_path"
+	
+	run_cmd_path="$HOME/.phycas/run_phycas.sh"
+	if ! test -f "$run_cmd_path"
+	then
+		echo "#!/bin/sh" > "$run_cmd_path"
+		echo "env_settings_path=\$HOME/.phycas/phycas_gui_env.sh" >> "$run_cmd_path"
+		echo "if test -f \$env_settings_path" >> "$run_cmd_path"
+		echo "then" >> "$run_cmd_path"
+		echo "    source \$env_settings_path || exit 1" >> "$run_cmd_path"
+		echo "fi" >> "$run_cmd_path"
+		echo "python -i -c \"from phycas import *\"" >> "$run_cmd_path"
+		chmod +x "$run_cmd_path"
+	fi
+	if ! test -f "$run_cmd_path"
+	then
+		echo "The mandatory file $run_cmd_path does not exist, and cannot be created (perhaps there is a directory in the way or you do not have permissions)."
+		exit 1
+	fi
+	$run_cmd_path
+else
+	echo "The mandatory directory $HOME/.phycas does not exist, and cannot be created (perhaps there is a file in the way or you do not have permissions to write to the directory $HOME)."
+	exit 1
+fi
 
