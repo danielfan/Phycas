046e56ba9a563729d74a7f24d0427d22719a0888
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-17 14:12:43 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@634 b774380d-c90d-0410-997c-aec342e5d6fd

---
+++ installer/mac/iterm/makeDMG.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+NAME=Phycas
+VERSION=1.0
+MASTER_DMG=$NAME-$VERSION.dmg
+SOURCE_DIR="/Users/mholder/Documents/projects/iterm/iTerm/build/Deployment/"
+set -x
+files="PhycasGUI.app" 
+if test -d "$PHYCAS_ROOT"
+then
+	cd $PHYCAS_ROOT/installer/mac/diskimage/
+	TEMPLATE_DMG="template.dmg"
+	bunzip2 -k ${TEMPLATE_DMG}.bz2
+	WC_DMG=wapp.dmg
+	WC_DIR=wapp
+	cp $TEMPLATE_DMG $WC_DMG
+	if test ./$WC_DIR
+	then
+		rm -rf ./$WC_DIR
+	fi
+	mkdir -p ${WC_DIR}
+	hdiutil attach "$WC_DMG" -noautoopen -quiet -mountpoint "$WC_DIR"
+	for i in $files
+	do
+		rm -rf "$WC_DIR/$i"; \
+		ditto -rsrc "$SOURCE_DIR/$i" "$WC_DIR/$i"; \
+	done
+	WC_DEV=`hdiutil info | grep "$WC_DIR" | grep "Apple_HFS" | awk '{print $1}'` && hdiutil detach $WC_DEV -quiet -force
+	rm -f "$MASTER_DMG"
+	hdiutil convert "$WC_DMG" -quiet -format UDZO -imagekey zlib-level=9 -o "$@"
+	#rm -rf $WC_DIR
+	mv "$WC_DMG" "$MASTER_DMG"
+else
+	echo "PHYCAS_ROOT is not defined as a directory"
+	exit 1
+fi
+exit 0
