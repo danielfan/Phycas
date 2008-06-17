fea958d9fe04dfc11cfc3d78fc38b47bad9986fa
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-17 15:56:53 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@639 b774380d-c90d-0410-997c-aec342e5d6fd

--- installer/mac/iterm/refill_phycas_gui_app.sh
+++ installer/mac/iterm/refill_phycas_gui_app.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -x
 tar xfjv PhycasGUI.app.tar.bz || exit 1
-cp PhycasGUIexecFromGUI.sh PhycasGUI.app/Contents/MacOS
-chmod +x PhycasGUI.app/Contents/MacOS/PhycasGUIexecFromGUI.sh
-cp -r $PHYCAS_ROOT/phycas PhycasGUI.app/Contents/Resources || exit 2
-find  PhycasGUI.app/Contents/Resources/ -name .svn -exec rm -rf {} \; 
+mv PhycasGUI.app Phycas.app
+cp PhycasGUIexecFromGUI.sh Phycas.app/Contents/MacOS
+chmod +x Phycas.app/Contents/MacOS/PhycasGUIexecFromGUI.sh
+cp -r $PHYCAS_ROOT/phycas Phycas.app/Contents/Resources || exit 2
+find  Phycas.app/Contents/Resources/ -name .svn -exec rm -rf {} \; 
 sh makeDMG.sh || exit 4
