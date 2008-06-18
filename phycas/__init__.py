d4aa0209001d802fc3dfa0a2d0a9ecc53e14655e
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-18 15:29:51 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@650 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/__init__.py
+++ phycas/__init__.py
@@ -131,11 +131,11 @@ class Sumt(PhycasCommand):
                    ("input_tree_file",     None,           "Set to the name of the input tree file. This setting should not be None at the time the sumt method is called."),
                    ("trees_prefix",        'sumt_trees',   "The output tree file in which all distinct tree topologies are saved along with the majority-rule consensus tree will be named <sumt_trees_prefix>.tre and the corresponding pdf file containing graphical representations of these trees will be named <sumt_trees_prefix>.pdf. This setting cannot be None when the sumt method is called."),
                    ("splits_prefix",       'sumt_splits',  "The pdf file showing plots depicting split posteriors through time and split sojourns will be named <sumt_splits_prefix>.pdf. If None, this analysis will be skipped."),
-                   ("output_replace",      False,          "If True, output files will be replaced automatically if they exist; if False, a random integer will be added to the name so that the name no longer matches an existing file"),
-                   ("burnin",              1,              "Number of trees to skip in sumt_input_tree_file"),
-                   ("equal_brlens",        False,          "If True, trees in pdf file will be drawn with branch lengths equal, making support values easier to see; if set to True, consider setting pdf_scalebar_position = None (scalebar is irrelevant in this case)"),
+                   ("output_replace",      False,          "If True, output files will be replaced automatically if they exist; if False, a random integer will be added to the name so that the name no longer matches an existing file", bool),
+                   ("burnin",              1,              "Number of trees to skip in sumt_input_tree_file", int),
+                   ("equal_brlens",        False,          "If True, trees in pdf file will be drawn with branch lengths equal, making support values easier to see; if set to True, consider setting pdf_scalebar_position = None (scalebar is irrelevant in this case)", bool),
                    ("tree_credible_prob",  0.95,           "Include just enough trees in the <sumt_trees_prefix>.tre and <sumt_trees_prefix>.pdf files such that the cumulative posterior probability is greater than this value"),
-                   ("rooted",              False,          "Set to True if trees in sumt_input_tree_file are rooted; otherwise, leave set to default value of False to assume trees are unrooted"),
+                   ("rooted",              False,          "Set to True if trees in sumt_input_tree_file are rooted; otherwise, leave set to default value of False to assume trees are unrooted", bool),
                 )
         PhycasCommand.__init__(self, p, args)
 
