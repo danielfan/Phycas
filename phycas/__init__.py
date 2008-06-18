4dc6bd4889241113812f6e19e806ec307c8aac02
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-18 15:20:18 +0000

    PhycasCmd added with validation of attribute settings and case-insensitivity
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@647 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/__init__.py
+++ phycas/__init__.py
@@ -18,6 +18,7 @@ import PDFGen
 import Phylogeny
 import ProbDist
 import ReadNexus
+import sys
 from Phycas import Phycas
 _user_ini_checked = False
 if not _user_ini_checked:
@@ -48,42 +49,69 @@ def ttysize():
     except:
         return None
 ###############################################################################
-
+def error_msg(msg):
+    sys.stderr.write("Error: %s\n" % msg)
 
 class PhycasCmdOpts(object):
     _help_str_wrapper = textwrap.TextWrapper()
     _help_fmt_str = "%-19s %-19s %s"
     
     def __init__(self, command, args):
+        self.current = {}
         self.help_info = {}
         self.optionsInOrder = args
+        self.command = command
         for opt in args:
             name, default, help_str = opt
-            command.__dict__[name] = default
             self.help_info[name] =  [default, help_str]
+            self.current[name] = default
+
     def __str__(self):
         PhycasCmdOpts._reset_term_width()
         opts_help = []
         for i in self.optionsInOrder:
             hs = "\n".join(PhycasCmdOpts._help_str_wrapper.wrap(i[2]))
-            s = PhycasCmdOpts._help_fmt_str % (i[0], i[1], hs[40:])
+            s = PhycasCmdOpts._help_fmt_str % (i[0], self.current[i[0]], hs[40:])
             opts_help.append(s)
         return "\n".join(opts_help)
-
+    def set_opt(self, name, value):
+        if name in self.current:
+            self.current[name] = value
+        else:
+            error_msg("%s does not contain an attribute %s" % (self.command.__class__.__name__, name))
     def _reset_term_width():
         tty_sz = ttysize()
         if tty_sz:
             PhycasCmdOpts._set_terminal_width(tty_sz[1])
     _reset_term_width = staticmethod(_reset_term_width)
+
     def _set_terminal_width(w):
         new_width = max(60, w)
         PhycasCmdOpts._help_str_wrapper.width = new_width
         PhycasCmdOpts._help_str_wrapper.subsequent_indent = " "*40
         PhycasCmdOpts._help_str_wrapper.initial_indent = " "*40
     _set_terminal_width = staticmethod(_set_terminal_width)
+
 PhycasCmdOpts._set_terminal_width(80)
 
-class Sumt:
+class PhycasCommand():
+    def __init__(self, phycas, option_defs):
+        self.__dict__["options"] = PhycasCmdOpts(self, option_defs)
+        self.__dict__["phycas"] = phycas
+    def __getattr__(self, name):
+        try:
+            return self.options.current[name.lower()]
+        except:
+            raise AttributeError("%s does not contain an attribute %s" % (self.__class__.__name__, name))
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            self.__dict__[name] = value
+        else:
+            self.__dict__["options"].set_opt(name.lower(), value)
+    def help(self, opt=None):
+       print self.options
+        
+class Sumt(PhycasCommand):
     def __init__(self, p):
         args = (   ("outgroup_taxon",      None,           "Set to the taxon name of the tip serving as the outgroup for display rooting purposes (note: at this time outgroup can consist of just one taxon)"),
                    ("input_tree_file",     None,           "Set to the name of the input tree file. This setting should not be None at the time the sumt method is called."),
@@ -95,8 +123,7 @@ class Sumt:
                    ("tree_credible_prob",  0.95,           "Include just enough trees in the <sumt_trees_prefix>.tre and <sumt_trees_prefix>.pdf files such that the cumulative posterior probability is greater than this value"),
                    ("rooted",              False,          "Set to True if trees in sumt_input_tree_file are rooted; otherwise, leave set to default value of False to assume trees are unrooted"),
                 )
-        self.options = PhycasCmdOpts(self, args)
-        self.phycas = p
+        PhycasCommand.__init__(self, p, args)
 
     def __call__(self):
         self.phycas.sumt_outgroup_taxon     = self.outgroup_taxon
@@ -109,9 +136,6 @@ class Sumt:
         self.phycas.sumt_tree_credible_prob = self.tree_credible_prob
         self.phycas.sumt_rooted             = self.rooted            
         self.phycas.sumt()
-        
-    def help(self, opt=None):
-       print self.options
        
 sumt = Sumt(phycas)
 
