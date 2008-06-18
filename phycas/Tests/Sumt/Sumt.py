7461deffafb06470a034968c4f7f937a818b5965
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-18 15:34:32 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@651 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Tests/Sumt/Sumt.py
+++ phycas/Tests/Sumt/Sumt.py
@@ -1,14 +1,13 @@
 from phycas import *
 
 if __name__ == '__main__':
-    phycas = Phycas()
     phycas.log_file_name = 'logfile.txt'
-    phycas.sumt_input_tree_file = 'test.t'
-    phycas.sumt_trees_prefix = 'trees'
-    phycas.sumt_splits_prefix = 'splits'
-    phycas.sumt_burnin = 11
-    phycas.sumt_outgroup_taxon = '40 Cyanophora paradoxa'
-    phycas.sumt_output_replace = True
-    phycas.sumt_tree_credible_prob = 1.0
-    phycas.sumt()
+    sumt.input_tree_file = 'test.t'
+    sumt.trees_prefix = 'trees'
+    sumt.splits_prefix = 'splits'
+    sumt.burnin = 11
+    sumt.outgroup_taxon = '40 Cyanophora paradoxa'
+    sumt.output_replace = True
+    sumt.tree_credible_prob = 1.0
+    sumt()
     
