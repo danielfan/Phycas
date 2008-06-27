aa42ec79b70b2511e337136099c1e4ad665e6321
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-06-27 03:57:28 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@701 b774380d-c90d-0410-997c-aec342e5d6fd

--- installer/mac/iterm/makeDMG.sh
+++ installer/mac/iterm/makeDMG.sh
@@ -6,51 +6,28 @@ set -x
 if test -d "$PHYCAS_ROOT"
 then
 	SOURCE_DIR="$PHYCAS_ROOT/installer/mac/iterm"
-	cd $PHYCAS_ROOT/installer/mac/diskimage/
-	TEMPLATE_DMG="template.dmg"
-	if test -f ${TEMPLATE_DMG}
+	cd $SOURCE_DIR || exit
+	if test -f $MASTER_DMG
 	then
-		rm ${TEMPLATE_DMG}
+		rm  $MASTER_DMG || exit
 	fi
-	bunzip2 -k ${TEMPLATE_DMG}.bz2
-	WC_DMG=wapp.dmg
-	WC_DIR=wapp
-	cp $TEMPLATE_DMG $WC_DMG
-	if test ./$WC_DIR
+	if test -d ./phycasimg
 	then
-		rm -rf ./$WC_DIR
+		rm -r ./phycasimg || exit
 	fi
-	mkdir -p ${WC_DIR}
-	hdiutil attach "$WC_DMG" -noautoopen -quiet -mountpoint "$WC_DIR"
+	mkdir phycasimg || exit
 	for i in "Phycas.app" 
 	do
-		rm -rf "$WC_DIR/$i"; \
-		ditto -rsrc "$SOURCE_DIR/$i" "$WC_DIR/$i"; \
+		ditto -rsrc "./$i" "phycasimg/$i" || exit
 	done
 	for i in "LICENSE" "readme.txt"
 	do
-		rm -rf "$WC_DIR/$i"; \
-		ditto -rsrc "$PHYCAS_ROOT/$i" "$WC_DIR/$i"; \
+		ditto -rsrc "$PHYCAS_ROOT/$i" "phycasimg/$i" || exit
 	done
-	WC_DEV=`hdiutil info | grep "$WC_DIR" | grep "Apple_HFS" | awk '{print $1}'` 
-	if test -z $WC_DEV
-	then
-		WC_DEV=`hdiutil info | grep "$WC_DIR" | grep '^/' | awk '{print $1}'` 
-	fi
-	if test -z $WC_DEV
-	then
-		echo "Could not unmount. Run 'hdiutil info' "
-		exit 2
-	else
-		hdiutil detach $WC_DEV -quiet -force
-		rm -f "$MASTER_DMG"
-		hdiutil convert "$WC_DMG" -quiet -format UDZO -imagekey zlib-level=9 -o "$@"
-		#rm -rf $WC_DIR
-		mv "$WC_DMG" "$MASTER_DMG"
-		mv "$MASTER_DMG" "$SOURCE_DIR/"
-	fi
+	hdiutil create -srcfolder phycas $MASTER_DMG
 else
 	echo "PHYCAS_ROOT is not defined as a directory"
 	exit 1
 fi
 exit 0
+
