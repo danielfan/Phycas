9e41189bfb046b6689ee82e5ffc94998218876fe
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-07-18 15:14:22 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@764 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Tests/Sumt/Sumt.py
+++ phycas/Tests/Sumt/Sumt.py
@@ -1,6 +1,13 @@
 from phycas import *
 
 if __name__ == '__main__':
+    # touch the output files so that they will always be replaced (otherwise
+    #   the produced output on a new machine will not have the "replacing <filename>
+    #   lines that the reference_output has and the test will fail.
+    touch("splits.pdf")
+    touch("trees.pdf")
+    touch("trees.tre")
+    
     phycas.log_file_name    = 'logfile.txt'
     sumt.trees              = 'test.t'
     sumt.out.trees.prefix   = "trees"
