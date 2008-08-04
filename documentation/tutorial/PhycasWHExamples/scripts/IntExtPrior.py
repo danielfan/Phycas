2f8c7ec1e3e446c9d11ee56622b13e7245b5cc6c
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2008-08-04 04:05:43 +0000

    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@888 b774380d-c90d-0410-997c-aec342e5d6fd

--- documentation/tutorial/PhycasWHExamples/scripts/IntExtPrior.py
+++ documentation/tutorial/PhycasWHExamples/scripts/IntExtPrior.py
@@ -14,13 +14,13 @@ model.gamma_shape_prior = Exponential(2.0)
 model.gamma_shape_prior.getMean()
 model.kappa_prior = Exponential(0.25)
 model.base_freq_param_prior = Gamma(1.0, 1.0)
-model.edgelen_hyperprior = InverseGamma(2.10000, 0.90909)
 
-mcmc.allow_polytomies = True
-mcmc.polytomy_prior = True
-import math
-mcmc.topo_prior_C = math.exp(1.0)
+model.edgelen_hyperprior = None
+model.internal_edgelen_prior = Exponential(10000.0)
+model.external_edgelen_prior = Exponential(10.0)
 
+
+mcmc.allow_polytomies = False
 
 rng = Lot()
 rng.setSeed(13957)
@@ -30,24 +30,24 @@ mcmc.starting_tree_source = randomtree(rng=rng)
 mcmc.ncycles = 2000
 mcmc.sample_every = 10
 
-mcmc.out.trees.prefix = "polytomy_trees"
+mcmc.out.trees.prefix = "int_ext_trees" 
 mcmc.out.trees.mode = REPLACE 
-mcmc.out.params.prefix = "polytomy_params"
+mcmc.out.params.prefix = "int_ext_params" 
 mcmc.out.params.mode = REPLACE 
-mcmc.out.log.prefix = "polytomy_output"
+mcmc.out.log.prefix = "int_ext_output" 
 mcmc.out.log.mode = REPLACE 
 mcmc.current()
 
 mcmc()
 
-sumt.outgroup_taxon = "Oedogonium cardiacum"
-sumt.trees = "polytomy_trees.t" 
+sumt.outgroup_taxon = "Oedogonium cardiacum" 
+sumt.trees = "int_ext_trees.t" 
 sumt.burnin = 101 
-sumt.out.log.prefix = "polytomy_sumt_output"
+sumt.out.log.prefix = "int_ext_sumt_output" 
 sumt.out.log.mode = APPEND 
-sumt.out.trees.prefix = "polytomy_sumt_trees"
+sumt.out.trees.prefix = "int_ext_sumt_trees" 
 sumt.out.trees.mode = REPLACE 
-sumt.out.splits.prefix = "polytomy_sumt_splits"
+sumt.out.splits.prefix = "int_ext_sumt_splits" 
 sumt.out.splits.mode = REPLACE 
 
 sumt()
