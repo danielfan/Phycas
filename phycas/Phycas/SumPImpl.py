3e4d1d709e1777fb3f27329433d3ce1b638f7d72
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-01-16 21:37:55 +0000

    Added calculation of autocorrelations and effective sample sizes to sump command (but still only for ps analyses)
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1003 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Phycas/SumPImpl.py
+++ phycas/Phycas/SumPImpl.py
@@ -3,6 +3,18 @@ from phycas import *
 from phycas.Utilities.PhycasCommand import *
 from phycas.Utilities.CommonFunctions import CommonFunctions
 
+class VarianceZeroError(Exception):
+    def __init__(self):
+        self.message = 'Cannot calculate autocorrelation because variance is zero'
+    def __str__(self):
+        return self.message
+    
+class InvalidNumberOfColumns(Exception):
+    def __init__(self, nparts, nexpected, line_num):
+        self.message = 'Number of values (%d) on line %d inconsistent with number of column headers (%d)' % (nparts, line_num, nexpected)
+    def __str__(self):
+        return self.message
+
 class ParamSummarizer(CommonFunctions):
     #---+----|----+----|----+----|----+----|----+----|----+----|----+----|
     """
@@ -54,10 +66,11 @@ class ParamSummarizer(CommonFunctions):
         # This approach uses Simpson's method to interpolate between beta values using the
         # interpolation polynomial in Lagrange form. This eliminates a big chunk of the bias
         # apparent with Lartillot and Phillippe method when the number of beta values used is small
-        K = len(betas)
+        nbetas = len(betas)
+        if nbetas < 3:
+            raise Exception("Must have at least 3 beta values to compute path sampling using Simpson's rule")
         marginal_like = 0.0
-        ps_aborted = False
-        for i in range(K - 2):
+        for i in range(nbetas - 2):
             x = [betas[i], betas[i+1], betas[i+2]]
             y = [means[i], means[i+1], means[i+2]]
             # print '\ni = %d (dx = %.5f, dy = %.5f)' % (i,x[0] - x[2], y[0] - y[2])
@@ -69,7 +82,7 @@ class ParamSummarizer(CommonFunctions):
                 marginal_like += a
                 b = self.cumLagrange(2, x, y)
                 marginal_like += (b - a)/2.0
-            elif i == K - 3:
+            elif i == nbetas - 3:
                 a = self.cumLagrange(1, x, y)
                 marginal_like += a/2.0
                 b = self.cumLagrange(2, x, y)
@@ -96,90 +109,175 @@ class ParamSummarizer(CommonFunctions):
         self.output(" %.8f Path sampling method (using Simpson's rule)" % (marginal_like))
 
     def ps_trapezoid(self, betas, means):
-        K = len(betas)
+        nbetas = len(betas)
         marginal_like = 0.0
-        ps_aborted = False
-        for i in range(K):
+        for i in range(nbetas):
             if i == 0:
                 before = betas[0]
             else:
                 before = betas[i-1]
-            if i == K - 1:
-                after = betas[K - 1]
+            if i == nbetas - 1:
+                after = betas[nbetas - 1]
             else:
                 after = betas[i+1]
             diff = before - after
             if diff < 0.0:
-                ps_aborted = True
-                ps_msg = 'Phycas does not currently support path sampling from prior toward posterior'
-                break
+                raise Exception('Phycas does not currently support path sampling from prior toward posterior')
             #print 'mean %d = %f (diff = %f)' % (i, means[i], diff)
             marginal_like += means[i]*diff/2.0
-        if ps_aborted:
-            self.output(ps_msg)
-        else:
-            self.output(' %.8f Path sampling method (using trapezoid rule)' % (marginal_like))
+        self.output(' %.8f Path sampling method (using trapezoid rule)' % (marginal_like))
                 
     def ss(self, betas, likes):
         # Calculate marginal likelihood using steppingstone sampling method
+        # betas is a list of beta values ordered from the first sampled to the last sampled
+        # likes is a map: i.e. likes[b] holds list of log-likelihoods sampled for beta value b
+        # Assumes that betas[0] = 1.0 and betas[-1] = 0.0
         lnR = 0.0
         seR = 0.0
         nbetas = len(betas)
+        if betas[0] != 1.0 or betas[-1] != 0.0:
+            raise Exception('Steppingstone sampling requires beta values to be ordered from 1.0 (first) to 0.0 (last)')
         for i in range(1,nbetas):
-            beta_incr = betas[i-1] - betas[i]
+            blarger = betas[i - 1]
+            bsmaller = betas[i]
+            beta_incr = blarger - bsmaller
+            loglikes = likes[bsmaller]
             tmp = 0.0
-            n = len(likes[i])
-            Lmax = max(likes[i])
-            for lnL in likes[i]:
+            n = len(loglikes)
+            Lmax = max(loglikes)
+            for lnL in loglikes:
                 tmp += math.exp(beta_incr*(lnL - Lmax))
             tmp /= float(n)
             lnR += beta_incr*Lmax + math.log(tmp)
         
             # standard error calculation
             tmp1 = 0.0
-            for lnL in likes[i]:
+            for lnL in loglikes:
                 aa = math.exp(beta_incr*(lnL - Lmax))/tmp
                 tmp1 += math.pow((aa - 1.0),2.0)
             tmp1 /= float(n)
             seR += tmp1/float(n)
         self.output(' %.8f Steppingstone sampling method (se = %.8f)' % (lnR, seR))
         
-    def marginal_likelihood(self, lines, burnin):
-        beta_sum = {}
-        beta_num = {}
-        beta_likes = {}
+    def autocorr_ess(self, values):
+        nvalues = len(values)
+        n = float(nvalues)
+        
+        # calculate the mean
+        m = sum(values)/n
+        
+        # calculate the variance
+        ss = 0.0
+        for x in values:
+            ss += x**2.0
+        var = (ss - n*m*m)/(n - 1.0)
+        
+        # calculate the covariance
+        cov = 0.0
+        for i in range(nvalues - 1):
+            x = values[i] - m
+            y = values[i+1] - m
+            cov += x*y
+        cov /= n - 1.0
+        if var <= 0.0:
+            raise VarianceZeroError()
+        r = cov/var
+        ess = n*(1.0 - r)/(1.0 + r)
+        return (r,ess)
+    
+    def marginal_likelihood(self, headers, lines, burnin):
+        # Create params such that, for example, params['lnL'][1.0] is a list of 
+        # all log-likelihood values sampled for beta = 1.0 (in the order in which
+        # they were sampled)
+        betas = []
+        params = {}
+        for h in headers:
+            params[h] = {}
         start = 2 + burnin
-        for line in lines[start:]:
+        curr_beta = None
+        for i,line in enumerate(lines[start:]):
             parts = line.split()
+            if len(parts) != len(headers):
+                raise InvalidNumberOfColumns(len(parts), len(headers), i + start + 1)
             beta = float(parts[1])
-            lnL  = float(parts[2])
-            try:
-                beta_sum[beta] += lnL
-                beta_num[beta] += 1
-                beta_likes[beta].append(lnL)
-            except KeyError:
-                beta_sum[beta] = lnL
-                beta_num[beta] = 1
-                beta_likes[beta] = [lnL]
-        beta_vect = beta_sum.keys()
-        beta_vect.sort()
-        beta_vect.reverse()
-        mean_vect = []
-        likes_vect = []
-        for b in beta_vect:
-            m = beta_sum[b]/float(beta_num[b])
-            mean_vect.append(m)
-            likes_vect.append(beta_likes[b])
-            
+            if curr_beta is None or curr_beta != beta:
+                curr_beta = beta
+                betas.append(beta)
+                for h,x in zip(headers,parts):
+                    params[h][beta] = [float(x)]
+            else:
+                for h,x in zip(headers,parts):
+                    params[h][beta].append(float(x))
+                    
+        # Output first-order autocorrelation for each parameter (and the log-likelihood)
+        # for each beta value separately
+        self.output('\nAutocorrelations (lag 1):\n')
+        self.output('%12s%s' % ('beta ->',' '.join(['%12.5f' % b for b in betas])))
+        for h in headers[2:]:   # skip 'Gen' and 'beta' headers
+            s = []
+            for b in betas:
+                p = params[h][b]
+                try:
+                    r,ess = self.autocorr_ess(p)
+                except VarianceZeroError:
+                    s.append('%12s' % '---')
+                else:
+                    s.append('%12.5f' % r)
+            self.output('%12s%s' % (h,' '.join(s)))
+
+        # Output effective sample size for each parameter (and the log-likelihood)
+        # for each beta value separately
+        actual_sample_size = len(params['LnL'][1.0])
+        all_same = True
+        self.output('\nEffective sample sizes (actual sample size = %d):\n' % actual_sample_size)
+        self.output('%12s%s' % ('beta ->',' '.join(['%12.5f' % b for b in betas])))
+        for h in headers[2:]:   # skip 'Gen' and 'beta' headers
+            s = []
+            for b in betas:
+                p = params[h][b]
+                n = len(p)
+                if n != actual_sample_size:
+                    all_same = False
+                try:
+                    r,ess = self.autocorr_ess(p)
+                except VarianceZeroError:
+                    s.append('%12s' % '---')
+                else:
+                    s.append('%12.1f' % ess)
+            self.output('%12s%s' % (h,' '.join(s)))
+        if not all_same:
+            self.output('\nWarning: actual sample sizes varied across beta values and/or parameters!')
+
+        # Compute means of log-likelihoods for each beta value (used for ps calculation)
+        means = []
+        for b in betas:
+            p = params['LnL'][b]
+            m = sum(p)/float(len(p))
+            means.append(m)
         self.output('\nMean log-likelihood for each value of beta used\nfor marginal likelihood estimation:\n')
         self.output('%12s %12s' % ('beta','mean lnL'))
-        for b,m in zip(beta_vect,mean_vect):
-            self.output('%12.5f %12.5f' % (b,m))
-        self.output('\nMarginal likelihood estimates:')
-        self.ss(beta_vect, likes_vect)
-        self.ps_trapezoid(beta_vect, mean_vect)
-        self.ps_simpsons(beta_vect, mean_vect)
+        for b,m in zip(betas, means):
+            s = ''
+            if b == 1.0:
+                s = '(posterior)'
+            elif b == 0.0:
+                s = '(prior)'
+            self.output('%12.5f %12.5f %s' % (b,m,s))
 
+        self.output('\nMarginal likelihood estimates:')
+        try:
+            self.ss(betas, params['LnL'])
+        except Exception,e:
+            self.output(' %s' % e.message)
+        try:
+            self.ps_trapezoid(betas, means)
+        except Exception,e:
+            self.output(' %s' % e.message)
+        try:
+            self.ps_simpsons(betas, means)
+        except Exception,e:
+            self.output(' %s' % e.message)
+    
     def std_summary(self, lines, burnin):
          print 'Standard summary is not yet implemented'
         
@@ -192,9 +290,12 @@ class ParamSummarizer(CommonFunctions):
         else:
             headers = lines[1].split()
             if headers[1] == 'beta':
-                self.marginal_likelihood(lines, burnin)
+                try:
+                    self.marginal_likelihood(headers, lines, burnin)
+                except InvalidNumberOfColumns, e:
+                    print e
             else:
-                self.std_summary(lines, burnin)
+                self.std_summary(headers, lines, burnin)
 
 # The stuff below here can be deleted at any time...            
 # def cumLagrangeWG(self, which, x, y):
--- phycas/Tests/Sump/Sump.py
+++ phycas/Tests/Sump/Sump.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     sump.out.log            = 'logfile.txt'
     sump.out.log.mode       = REPLACE
     sump.file               = 'test.p'
+    #sump.file               = 'k2.n10k.p'
     sump.burnin             = 1
     sump()
     
