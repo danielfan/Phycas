4013aba5c98bf94f69093e0934b221ca7ab0e88b
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-02-07 01:20:33 +0000

    commented out code in makeDMG that adds Woods Hole tutorial to dmg
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1008 b774380d-c90d-0410-997c-aec342e5d6fd

--- installer/mac/iterm/makeDMG.sh
+++ installer/mac/iterm/makeDMG.sh
@@ -23,18 +23,18 @@ then
 	ditto -rsrc "$PHYCAS_ROOT/LICENSE" "phycasimg/LICENSE" || exit
 	ditto -rsrc "README" "phycasimg/README" || exit
 	ditto -rsrc "$PHYCAS_ROOT/documentation/users/manual.pdf" "phycasimg/manual.pdf" || exit
-	mkdir "phycasimg/PhycasTutorial"
-	mkdir "phycasimg/PhycasTutorial/scripts"
-	for f in $PHYCAS_ROOT/phycas/Examples/Tutorial/PhycasWHExamples/*nex
-	do 
-		ditto -rsrc "$f" "phycasimg/PhycasTutorial" || exit
-	done
-	for f in $PHYCAS_ROOT/phycas/Examples/Tutorial/PhycasWHExamples/scripts/*py
-	do 
-		ditto -rsrc "$f" "phycasimg/PhycasTutorial/scripts/" || exit
-		ditto -rsrc "$f" "phycasimg/PhycasTutorial/" || exit
-	done
-	ditto -rsrc "$PHYCAS_ROOT/phycas/Examples/Tutorial/phycas_woods_hole_08.pdf" "phycasimg/PhycasTutorial/PhycasTutorial.pdf" || exit
+	# mkdir "phycasimg/PhycasTutorial"
+	# mkdir "phycasimg/PhycasTutorial/scripts"
+	# for f in $PHYCAS_ROOT/phycas/Examples/Tutorial/PhycasWHExamples/*nex
+	# do 
+	# 	ditto -rsrc "$f" "phycasimg/PhycasTutorial" || exit
+	# done
+	# for f in $PHYCAS_ROOT/phycas/Examples/Tutorial/PhycasWHExamples/scripts/*py
+	# do 
+	# 	ditto -rsrc "$f" "phycasimg/PhycasTutorial/scripts/" || exit
+	# 	ditto -rsrc "$f" "phycasimg/PhycasTutorial/" || exit
+	# done
+	# ditto -rsrc "$PHYCAS_ROOT/phycas/Examples/Tutorial/phycas_woods_hole_08.pdf" "phycasimg/PhycasTutorial/PhycasTutorial.pdf" || exit
 	
 	hdiutil create -srcfolder phycasimg $MASTER_DMG
 else
