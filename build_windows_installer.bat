3b4dbeeb0479896d51706609fbbc38037366ecd5
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-03-12 20:22:37 +0000

    Modified comments in build_windows_installer.bat
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1042 b774380d-c90d-0410-997c-aec342e5d6fd

--- build_windows_installer.bat
+++ build_windows_installer.bat
@@ -1,7 +1,7 @@
 @echo off
 REM
 REM Steps to creating a Windows distribution:
-REM   1. Build both boost_python.dll and Phycas using dojam.py
+REM   1. Build Phycas using bjam
 REM   2. Make sure "package_data=windows_package_data" is uncommented in setup.py
 REM   3. Change --target-version below to reflect correct Python target version
 REM   4. Run this batch file to generate installer (which will be in dist folder)
@@ -10,7 +10,7 @@ rmdir /s /q build
 rmdir /s /q dist
 REM python setup.py sdist --force-manifest
 REM copy /Y bin\boost\libs\python\build\boost_python.dll\vc-7_1\release\threading-multi\boost_python.dll phycas\Conversions
-python setup.py bdist_wininst --target-version 2.5 --bitmap phycaslogo.bmp --install-script win_shortcuts.py
+python setup.py bdist_wininst --target-version 2.6 --bitmap phycaslogo.bmp --install-script win_shortcuts.py
 del phycas\__init__.py 
 svn update
 pause
