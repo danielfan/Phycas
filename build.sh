a6da490cdc2ba86f91d032ec9caae8ecfa813460
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-03-12 17:51:34 +0000

    Corrected build.sh to reflect correct NCL version that will be bundled with Phycas
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1034 b774380d-c90d-0410-997c-aec342e5d6fd

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export OSTYPE="darwin"
+export OSTYPE="linux"
 export BOOST_ROOT="$HOME/boost_1_37_0"
-export PATH="${PATH}:$BOOST_ROOT/tools/jam/src/bin.macosxx86"
+export PATH="${PATH}:$BOOST_ROOT/tools/jam/src/bin.linuxx86"
 export BOOST_BUILD_PATH="${BOOST_ROOT}/tools/build/v2"
-export NCL_INSTALL_DIR="../ncl-2.1.382"
+export NCL_INSTALL_DIR="../ncl-2.2.414"
 export PHYCAS_ROOT="."
 bjam release
