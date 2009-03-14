b806822c44a2c876e91357739ba09a27b0162031
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-03-14 19:16:30 +0000

    Added a polytomous tree likelihood calculation to the existing non-polytomous tree likelihood calcualtion in the Underflow test
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1056 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Tests/Underflow/Underflow.py
+++ phycas/Tests/Underflow/Underflow.py
@@ -1,6 +1,11 @@
 import math
 from phycas import *
 
+outf = open('output.txt', 'w')
+
+# Calculate likelihood under conditions in which underflow correction is used
+# for a tree without polytomies
+
 model.type = 'gtr'
 model.pinvar_model = True
 model.edgelen_hyperprior = None
@@ -15,10 +20,35 @@ blob = readFile(filename)
 like.data_source = blob.characters
 like.tree_source = TreeCollection(filename='gtrig.rbcL50.best.tre')
 like.starting_edgelen_dist = None
-like.uf_num_edges = 10
+like.uf_num_edges = 5
 like.store_site_likes = True
 lnL = like()
 
-outf = open('output.txt', 'w')
-outf.write('correct lnL = -18117.830737\n        lnL = %.6f\n\n' % lnL)
+outf.write('Computing likelihood for non-polytomous tree:\n')
+outf.write('  correct lnL = -18117.830737\n')
+outf.write('          lnL = %.6f\n\n' % lnL)
+
+# Calculate likelihood under conditions in which underflow correction is used
+# for a tree that has polytomies
+
+model.type      = 'hky'   # use the Hasegawa-Kishino-Yano (1985) model
+model.pinvar_model = False
+model.num_rates = 4        # add discrete gamma rate heterogeneity with 4 rate categories
+model.state_freqs = [0.25, 0.25, 0.25, 0.25]    # replace with actual value 
+model.kappa = 4.0                               # replace with actual value 
+model.gamma_shape = 0.5                         # replace with actual value 
+
+filename = getPhycasTestData('ShoupLewis.nex')
+blob = readFile(filename)
+like.data_source = blob.characters
+like.tree_source = TreeCollection(filename='polytomous.tre')
+like.starting_edgelen_dist = None
+like.uf_num_edges = 5
+like.store_site_likes = True
+lnL = like()
+
+outf.write('Computing likelihood for tree with polytomies:\n')
+outf.write('  correct lnL = -16403.967004359674\n')
+outf.write('          lnL = %.6f\n\n' % lnL)
+
 outf.close()
