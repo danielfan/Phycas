dd9c7761c0254a8ad2367ac2e7d83aba73441713
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-05-18 20:04:54 +0000

    adding unimap example in inprep folder
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1078 b774380d-c90d-0410-997c-aec342e5d6fd

---
+++ inprep/unimap/Unimap.py
@@ -0,0 +1,25 @@
+from phycas import *
+
+model.type                  = 'hky'
+
+mcmc.use_unimap             = True
+mcmc.outfile_prefix         = 'unitest'
+mcmc.random_seed            = 19375
+mcmc.data_source         = 'green.nex'
+mcmc.starting_tree_source   = 'random'
+mcmc.ncycles                = 100
+mcmc.sample_every           = 1
+mcmc.report_every           = 10
+mcmc.mapping_move_weight    = 1
+mcmc.unimap_nni_move_weight = 1
+mcmc.tree_scaler_weight     = 0
+mcmc.slice_weight           = 1
+mcmc.debugging              = True
+
+if False:
+	import sys,os
+	if os.path.basename(sys.executable) == 'python_d.exe':
+		raw_input('debug stop')
+
+mcmc()
+
