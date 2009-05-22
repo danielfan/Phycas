3826c78caa9c4488fd5c467241fa8a8e7914bd91
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-05-22 03:11:18 +0000

    appears to work if - and only if - the unimap_edge_move_weight  is disabled
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1103 b774380d-c90d-0410-997c-aec342e5d6fd

--- inprep/unimap/UnimapTest.py
+++ inprep/unimap/UnimapTest.py
@@ -10,7 +10,7 @@ mcmc.data_source         		= 'green.nex'
 # mcmc.starting_tree_source   	= TreeCollection(newick='(1:0.096451,2:0.059414,(3:0.069940,(4:0.065929,((5:0.066647,8:0.097704):0.010055,(6:0.063657,((7:0.046537,9:0.052228):0.007358,10:0.059386):0.014523):0.012032):0.008731):0.009339):0.007606)')
 #mcmc.starting_tree_source   	= TreeCollection(newick='(1:0.10000,2:0.15000,(3:0.02500,4:0.15000):0.05000)')
 mcmc.starting_tree_source   	= randomtree() # TreeCollection(newick='(1:0.020000,2:0.0100,(3:0.0200,4:0.02000):0.02000)')
-mcmc.ncycles                	= 2000
+mcmc.ncycles                	= 20000
 mcmc.nchains					= 1
 mcmc.sample_every           	= 1
 mcmc.report_every           	= 1
