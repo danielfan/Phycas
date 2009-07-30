8530fa0c69816cb6b19cf1eabbedb8f024d33cfc
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-07-30 14:15:31 +0000

    copied 08 tutorial to 09
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1148 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Examples/Tutorial/rebuild.sh
+++ phycas/Examples/Tutorial/rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x 
-pdflatex phycas_woods_hole_08.tex 
-bibtex phycas_woods_hole_08
-pdflatex phycas_woods_hole_08.tex 
-pdflatex phycas_woods_hole_08.tex 
-pdflatex phycas_woods_hole_08.tex 
+pdflatex phycas_woods_hole_09.tex 
+bibtex phycas_woods_hole_09
+pdflatex phycas_woods_hole_09.tex 
+pdflatex phycas_woods_hole_09.tex 
+pdflatex phycas_woods_hole_09.tex 
