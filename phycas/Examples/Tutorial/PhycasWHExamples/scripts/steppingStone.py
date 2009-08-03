82fc4ce60cec40e16241f3af84f9546a75201c7b
mholder <mholder@b774380d-c90d-0410-997c-aec342e5d6fd>
2009-08-03 19:01:32 +0000

    commenting the ss tutorial more thorouhly
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1158 b774380d-c90d-0410-997c-aec342e5d6fd

--- phycas/Examples/Tutorial/PhycasWHExamples/scripts/steppingStone.py
+++ phycas/Examples/Tutorial/PhycasWHExamples/scripts/steppingStone.py
@@ -1,119 +1,125 @@
-# This script will regenerate the information used for the points corresponding to 
-# K = 2 in Figure 10 of this paper:
+# This script demonstrates the use of the steppingstone sampler developed by:
 #
 # Xie, W., P. O. Lewis, Y. Fan, L. Kuo and M.-H. Chen. 2009.
 # Improving Marginal Likelihood Estimation for Bayesian Phylogenetic 
 # Model Selection. Syst. Biol., submitted Feb. 6, 2009.
-#
-# This figure plots estimates of the marginal likelihood (y-axis) for several
-# different values of K, where K is the number of beta intervals used. See
-# the Xie et al. paper for details.
-#
-# Note that this same script can be used to create both the HM estimate (call "mcmc()")
-# as well as the estimates for SS and PS (call "ss()"). 
 
-import math, sys
 from phycas import *
 
-#Toggling the following variables determines whether or not you use:
-# GTR vs HKY, and
-# Stepping stone vs harmonic mean estimator of the marginal likelihood
-use_gtr = True
-using_stepping_stone = True
+# The following variables are python variables that are not used in the rest
+#   of Phycas.  Rather they are used within this script to make it easier to 
+#   tweak the analysis and run it in a different configuration.
+use_gtr = False              # Set this to True for GTR and False for HKY 
 
+using_steppingstone = False # Set this to True for the steppingstone sample
+                            #   or False to run a typical MCMC analysis and 
+                            #   report the harmonic mean.
 
 # specify the data file name
 mcmc.data_source = 'simhky.nex'
 
 
-# settings relating to substitution model
 
+
+# Settings relating to substitution model
+# This demonstrates how we can use an if/else conditional statement in Python
+#   note that the indentation of the code matters here.
 if use_gtr:
     model.type = 'gtr'
-    model.relrates = [1.0, 4.0, 1.0, 1.0, 4.0, 1.0] # AC, AG, AT, CG, CT, GT
+    model.relrates = [1.0, 4.0, 1.0, 1.0, 4.0, 1.0] # These will be the initial
+                                                    # exchangeability rates, in the 
+                                                    # order AC, AG, AT, CG, CT, GT
+    # now we need to specify a prior:
+    model.relrate_prior = ProbDist.Dirichlet((1.0,1.0,1.0,1.0,1.0,1.0)) 
 else:
     model.type = 'hky'
-    model.kappa = 4.0
+    model.kappa = 4.0 # this is initial value of the transitition/transversion rate ratio.
+    model.kappa_prior = Exponential(1.0)
 
 
-model.num_rates = 1 # use number > 1 for discrete gamma rate heterogeneity
-model.pinvar_model = False # use True for proportion invariable sites model
-if model.num_rates > 1:
-    model.gamma_shape = 0.5
+model.num_rates = 1        # We won't worry with gamma-distributed rate het in this example
+model.pinvar_model = False # ... or the invariant sites model.
 
-model.state_freqs = [0.25, 0.25, 0.25, 0.25] # order is A,C,G,T
-
-# settings relating to prior distributions
-model.edgelen_hyperprior = None
-model.edgelen_prior = ProbDist.Exponential(1.0)
-model.relrate_prior = ProbDist.Dirichlet((1.0,1.0,1.0,1.0,1.0,1.0)) 
-model.kappa_prior = Exponential(1.0)
+# Set the starting base frequencies. The order in the list  is A,C,G,T,
+model.state_freqs = [0.25, 0.25, 0.25, 0.25] 
 model.state_freq_prior = Dirichlet((1.00000, 1.00000, 1.00000, 1.00000))
 
-# general settings 
+# settings relating to prior distributions
+model.edgelen_prior = ProbDist.Exponential(1.0) # We'll use a fixed prior on the branch lengths
+model.edgelen_hyperprior = None                 # ... not a hierarchical model.
+
+# Create a random number generator and pass it to the mcmc command so that 
+#   we can recreate this run if we want to later.
 rng = Lot()
 rng.setSeed(13957)
 mcmc.rng = rng
-mcmc.burnin = 100 # number of cycles before any sampling is done
-mcmc.ncycles = 1000 # number of MCMC cycles per value of beta. This is not enough, We are just using this for demo purposes
-mcmc.sample_every = 10 # log-likelihood will be sampled whenever cycle modulo ps_sample_every equals 0
-mcmc.report_every = 100
-mcmc.adapt_first = 2 
+
+
+# The following are general MCMC settings (not steppingstone sampling related).
+mcmc.burnin = 100       # discard the first 100 cycles
+mcmc.ncycles = 10000    # run the sample 10,000 generations at each value of beta 
+mcmc.sample_every = 10  # sample the tree every 10 cycles
+mcmc.report_every = 100 # report the status every 100 cycles
+mcmc.adapt_first = 20   # tune the slice sampler after 10 cycles
 mcmc.verbose = True
+mcmc.nchains = 1        # MCMCMC multiple chain analyses are not yet working
 
 
-# specify the starting tree
+# specify the starting tree. We'll use the model tree just so we can get 
+#   decent results from a very short run in the lab.
 mcmc.starting_tree_source = TreeCollection(newick='(t1:0.074783,(((((((t2:0.017390,t3:0.013871):0.014610,t4:0.038516):0.023079,t5:0.033673):0.011254,((t6:0.000718,t7:0):0.001124,t8:0.008383):0.020560):0.022048,(t14:0.127203,t15:0.017124):0.024730):0.002278,((t9:0.048058,t10:0.059900):0.024875,((t11:0.038944,t12:0.025554):0.014442,t13:0.034559):0.022475):0.002477):0.006325,t16:0.023526):0.006762,t17:0.025302)')
 
-# probably best to *not* change any of these
-mcmc.nchains = 1 # multiple chain analyses are not yet working
-mcmc.slice_weight = 1 # means one slice sampling update of each model parameter per cycle
 
 # fix the tree topology and use EdgeMove (which only change edge lengths) instead
 # of LargetSimonMove (which changes both edge lengths and the tree topology)
-mcmc.fix_topology           = True
-mcmc.edge_move_weight       = 1      # do 100 EdgeMove updates per cycle (makes sense to set this to some multiple of the number of edges)
+mcmc.fix_topology = True
 
 
-# specify how many times per cycle to rescale the entire tree
-mcmc.tree_scaler_weight     = 1
+# The next lines control how often different moves are performed during MCMC. 
+#   The integers are the number of times the move is used per-cycle
+mcmc.slice_weight = 1 
+mcmc.edge_move_weight = 1      
+mcmc.tree_scaler_weight = 1
 
 # specify whether to update relative rates individually using slice sampling (True) 
 # or jointly using a Metropolis-Hastings proposal (False)
 model.update_relrates_separately = False
-
-# specify how many times per cycle to propose new relative rates vector
 mcmc.rel_rate_weight        = 1
 
 # specify whether to update base frequencies individually using slice sampling (True) 
 # or jointly using a Metropolis-Hastings proposal (False)
 model.update_freqs_separately = False
-
-# specify how many times per cycle to propose a new vector of nucleotide frequencies
 mcmc.state_freq_weight      = 1
 
-# probably best to *not* change any of these
-mcmc.nchains                = 1         # multiple chain analyses are not yet working
-mcmc.slice_weight           = 1         # means one slice sampling update of each model parameter per cycle
 
+# settings specific to path sampling and steppingstone sampling
+# We'll be running MCMC with the likelihood raised to a power of beta for 
+#   several values of beta.
+ss.nbetavals = 20 # number of beta values
+ss.minbeta = 0.0 # smallest beta value to be sampled. 0 means sampling from the prior.
+ss.maxbeta = 1.0 # largest beta value to be sampled. 1 means sampling from the posterior.
 
-# settings specific to path/steppingstone sampling
-ss.nbetavals = 5 # number of beta values 
-ss.minbeta = 0.0 # smallest beta value to be sampled
-ss.maxbeta = 1.0 # largest beta value to be sampled
+# We pick the values of beta to use by calculating the quantiles of a 
+#   distribution (confusingly, we use a Beta distribution).  The
+#   next parameters specify a Beta(0.3, 1.0) distribution which
 ss.shape1 = 0.3 # first shape parameter of the beta sampling distribution
 ss.shape2 = 1.0 # second shape parameter of the beta sampling distribution
+
+
+
 
 # specify whether to use Metropolis-Hastings/slice sampling when exploring the prior (False)
 # or to sample directly from the prior (True)
 mcmc.draw_directly_from_prior = True
 
+
 # specify tuning parameters for the Metropolist-Hastings proposals
 # Note: there are two versions of each tuning parameter, one for the posterior
-# and one for the prior (denoted 0).
+#   and one for the prior (denoted 0).
 # When exploring distribtions intermediate between prior and posterior, an 
-# intermediate tuning parameter is used
-# Using bolder moves for distributions that are more similar to the prior improves mixing
+#   intermediate tuning parameter is used
+# Using bolder moves for distributions that are more similar to the prior
+#   improves mixing, so that we fully explore these `heated` landscapes
 mcmc.tree_scaler_lambda = 0.5 # min_lambda
 mcmc.tree_scaler_lambda0 = 1.0 # max_lambda
 mcmc.rel_rate_psi = 200.0 # max_psi
@@ -124,8 +130,11 @@ mcmc.edge_move_lambda     = 0.5     # min_lambda
 mcmc.edge_move_lambda0    = 1.0     # max_lambda
 
 
-# This will be the prefix for the name of output files generated
-if using_stepping_stone:
+# This will be the prefix for the name of output files generated.
+#   In the prefix we`ll indicate the model and whether or not we are 
+#   running a steppingstone analysis or a harmonic mean run so that we can tell
+#   what we ran later.
+if using_steppingstone:
     fnprefix = model.type + 'stepstone'
 else:
     fnprefix = model.type + 'harmonic'
@@ -144,13 +153,27 @@ mcmc.out.trees.prefix = fnprefix
 
 sump.out.log.prefix = 'sumt' + fnprefix
 
-if using_stepping_stone:
+if using_steppingstone:
     # start the MCMC analysis that will generate samples appropriate
-    # for both path sampling and steppingstone sampling
+    #   for both path sampling and steppingstone sampling
     ss()
 else:
+    # If we want to compare to the harmonic mean estimator we should at least
+    #   run that sampler an approximately similar amount of time.  Since we
+    #   will use the mcmc() function rather than ss(), the sampler will
+    #   not be examining different beta values.  It will only sample the 
+    #   posterior.  So we'll multiple the number of cycles by our 
+    #   nbetavals.  If you have not changed the number above, this means that our
+    #   MCMC will be 20 times as many cycles as the MCMC run on the posterior that
+    #   we conducted in the steppingstone version.
     mcmc.ncycles *= ss.nbetavals
     mcmc()
 
 
-sump(burnin=1,file="%s.p" % fnprefix)
+# The sump command actually does the steppingstone sampling (it does
+#   importance sampling of the likelihoods for the stepping stones) and the
+#   path sampling.
+# It will perform these calculations if the .p files have the format that
+#   indicates that we have done several beta values.
+sump_filename = fnprefix + ".p"
+sump(burnin=1,file=sump_filename)
