ba0b147123754b803807fd22677565cdc33b1dbc
polewis <polewis@b774380d-c90d-0410-997c-aec342e5d6fd>
2010-07-30 15:48:37 +0000

    More tweaks to build_windows_installer.bat to adjust it to its new home in installer/win
    git-svn-id: https://phycas.svn.sourceforge.net/svnroot/phycas/trunk@1344 b774380d-c90d-0410-997c-aec342e5d6fd

--- installer/win/build_windows_installer.bat
+++ installer/win/build_windows_installer.bat
@@ -6,12 +6,13 @@ REM   2. Make sure "package_data=windows_package_data" is uncommented in setup.p
 REM   3. Change --target-version below to reflect correct Python target version
 REM   4. Run this batch file to generate installer (which will be in dist folder)
 REM
+REM cd ..\..
 rmdir /s /q build
 rmdir /s /q dist
 REM python setup.py sdist --force-manifest
 REM copy /Y bin\boost\libs\python\build\boost_python.dll\vc-7_1\release\threading-multi\boost_python.dll phycas\Conversions
-cd ..\..
 python setup.py bdist_wininst --target-version 2.7 --bitmap phycaslogo.bmp --install-script win_shortcuts.py
 REM del phycas\__init__.py 
 REM svn update
+REM cd installer\win
 pause
