63039e0670a5f706cb32a6444c83e587cabe8a0d
Paul O. Lewis <paul.lewis@uconn.edu>
2010-08-09 09:37:10 -0400

    Changed sdist functionality
    Moved most sdist functionality out of setup.py and into MANIFEST.in, which is now put together from MANIFEST.common and either MANIFEST.linux or MANIFEST.windows just prior to calling "python setup.py sdist" in build_linux_source_distr.sh or build_windows_source_distr.sh.

---
+++ build_template.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This removes dynamic link libraries from previous builds
+./remove_dylibs.sh
+
+# Specify "linux" or "darwin" (for macos)
+export OSTYPE="linux"
+
+# The path to your boost installation (download from http://www.boost.org/) 
+export BOOST_ROOT="$HOME/boost_1_42_0"
+
+# Modify PATH so that bjam executable can be found
+# First, create bjam executable by running $BOOST_ROOT/tools/jam/src/build.sh
+# The build.sh script will create a macosxx86_64 folder on mac snow leopard
+# and a linuxx86 folder on linux. Be sure final part of path below is correct
+# for your platform.
+export PATH="${PATH}:$BOOST_ROOT/tools/jam/src/bin.macosxx86_64"
+
+# This is needed for bjam to find its way
+export BOOST_BUILD_PATH="${BOOST_ROOT}/tools/build/v2"
+
+# Provide path to Phycas source code
+export PHYCAS_ROOT="$HOME/Phycas-1.2.0"
+
+# Provide path to preinstalled Nexus Class Library 
+# (download from http://sourceforge.net/projects/ncl/)
+export NCL_ALREADY_INSTALLED=1
+export NCL_INSTALL_DIR="/usr/local"
+
+# Compile debug version if "debug" specified on command line
+# Otherwise, compile release version
+if [[ $# -gt 0 ]] && [[ $1 = "debug" ]]
+then
+	echo "*** COMPILING DEBUG VERSION ***"
+	bjam debug
+else
+	bjam release
+fi
