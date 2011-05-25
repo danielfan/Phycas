e9a8d0c7e17df054f7802b4266048857c37ec866
Dave Swofford <david.swofford@duke.edu>
2011-05-25 11:55:51 -0500

    project updates

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export OSTYPE="linux"
-export BOOST_ROOT="$HOME/boost_1_37_0"
-export PATH="${PATH}:$BOOST_ROOT/tools/jam/src/bin.linuxx86"
+export OSTYPE="darwin"
+export BOOST_ROOT="$HOME/tools/boost_1_41_0"
+export PATH="${PATH}:$BOOST_ROOT/tools/jam/src/bin.macosxx86_64"
 export BOOST_BUILD_PATH="${BOOST_ROOT}/tools/build/v2"
-export NCL_INSTALL_DIR="../ncl-2.2.414"
+export NCL_INSTALL_DIR="../ncl-svn/branches/v2.2"
 export PHYCAS_ROOT="."
 bjam release
