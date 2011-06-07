93938390a5048d495b769d938b82de9db829d5e0
Mark T. Holder <mtholder@gmail.com>
2011-06-07 17:05:52 -0500

    code for making the reference distribution less peaked - hopefully this will result in better efficiency

--- inprep/rokas8/multitree/broaden_dist.py
+++ inprep/rokas8/multitree/broaden_dist.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 from phycas import *
 import sys
-fn = sys.argv[1]
-coeff_var = float(sys.argv[2])
-for line in open(fn, 'rU'):
+coeff_var = float(sys.argv[1])
+if len(sys.argv) > 2:
+    fn = sys.argv[2]
+    stream = open(fn, 'rU')
+else:
+    stream = sys.stdin
+for line in stream:
     ls = line.split('=')
     if len(ls) == 1:
         print line.strip()
     else:
         key= ls[0].strip()
-        value = eval('='.join(ls[1:]).strip())
-        mean = value.getMean()
+        old_dist = eval('='.join(ls[1:]).strip())
+        mean = old_dist.getMean()
+        old_sd = old_dist.getStdDev()
         sd = coeff_var*mean
-        new_dist = Gamma(mean=mean, std_dev=sd)
-        print key,'=', new_dist
+        if sd > old_sd:
+            new_dist = Gamma(mean=mean, std_dev=sd)
+            comment = '# mean kept at ' + str(mean) + ' std_dev changed from '+ str(old_sd) + ' to ' + str(sd)
+        else:
+            sd = old_sd
+            new_dist = old_dist
+            comment = ''
+        print key,'=', new_dist, comment
         
--- inprep/rokas8/multitree/do_sweep.sh
+++ inprep/rokas8/multitree/do_sweep.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
+cv="$1"
+if test -z $cv
+then
+    echo "Expecting a minimum coefficient of variation to apply to the parameters in the reference distribution"
+    exit 1
+fi
+cap="$2"
+if test -z $cap
+then
+    echo "Expecting a maximum split frequency to apply to every internal edge in the reference distribution"
+    exit 1
+fi
 set -x
 for s in 1 0.999 0.99 0.98 0.97 0.96 0.95 0.9 0.8 0.7 0.6 0.5 0.25 0.1
 do
-    if ! test -d scale${s}
+    dir="mincv${cv}_scale${s}_cap${cap}"
+    if ! test -d "$dir"
     then
-        mkdir scale${s}
+        mkdir "${dir}" || exit 
     fi
-    head -n1 ../pilot/refdist.txt | python 1010511/alter_edge_lengths.py -i --scale=${s} | awk '{print $1}' > scale${s}/refdist.txt
-    cat ../pilot/refdist_params.txt >> scale${s}/refdist.txt
-    echo 'master_edgelen = Gamma(1.0, 1.0)' >> scale${s}/refdist.txt
-    cat ../pilot/sump.refdist.txt >> scale${s}/refdist.txt
+    fn="${dir}/refdist.txt"
+    head -n1 ../pilot/refdist.txt | python 1010511/alter_edge_lengths.py -i --scale=${s} --cap=${cap} | awk '{print $1}' > "${fn}" || exit
+    cat ../pilot/refdist_params.txt >> "${fn}" || exit
+    # master_edgelen is not used, but currently required - @TODO
+    echo 'master_edgelen = Gamma(1.0, 1.0)' >> "${fn}" || exit
+    cat ../pilot/sump.refdist.txt >> "${fn}" || exit
 done
-
+set +x
+echo "Assured that parameters had a coefficient of variation of at least ${cv}"
+echo "and capped the upper limit on a split probability at ${cap}"
