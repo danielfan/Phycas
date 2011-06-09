393544eeb51c7858df260a6adf84b2ef0d1e5430
Mark T. Holder <mtholder@gmail.com>
2011-06-09 16:38:35 -0500

    tallies total prob

--- inprep/rokas4/from_brute_force.py
+++ inprep/rokas4/from_brute_force.py
@@ -25,10 +25,14 @@ log_marg_like = maxlnl + math.log(sum_diffs) - math.log(n)
 print 'log(marginal likelihood) =', log_marg_like
 print 'based on %d distinct tree topologies' % len(marglikes)
 log_total_marg_like = maxlnl + math.log(sum_diffs)
+print 'posterior probs'
+tot = 0.0
 for i in range(len(mv)):
    t = i + 1
    if t in marglikes.keys():
        prob = math.exp(marglikes[t] - log_total_marg_like)
+       tot += prob
        print '%d\t%g' % (t, prob)
    else:
        print '%d\t<not available>' % (t,)
+print 'Total prob =', tot
